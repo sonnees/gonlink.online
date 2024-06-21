085720d54ff3a134e8008a49717c4105f656673b
sonnees <sson12131415@gmail.com>
2024-06-21 22:32:12 +0700

    account-service: Traffic

--- backend/account-service/src/test/java/rutlink/online/accountservice/repository/TrafficRepositoryTest.java
+++ backend/account-service/src/test/java/rutlink/online/accountservice/repository/TrafficRepositoryTest.java
@@ -2,34 +2,57 @@ package rutlink.online.accountservice.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import rutlink.online.accountservice.entity.Traffic;
 
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TrafficRepositoryTest {
 
-    @Autowired
-    TrafficRepository trafficRepository;
+    @Autowired TrafficRepository trafficRepository;
+    @Autowired @Qualifier("simpleDateFormat")
+    SimpleDateFormat simpleDateFormat;
+    @Autowired @Qualifier("simpleDateFormatWithTime")
+    SimpleDateFormat simpleDateFormatWithTime;
+    @Autowired DateTimeFormatter dateTimeFormatter;
 
     @Test
     void save() {
         String shortCode = "abcdef";
-        Traffic traffic = new Traffic(shortCode);
+        String date = simpleDateFormat.format(Date.from(ZonedDateTime.now().toInstant()));
+
+        Traffic traffic = new Traffic(shortCode, date);
 
         Traffic save = trafficRepository.save(traffic);
-
         assertNotNull(save);
     }
 
     @Test
     void findTrafficByShortCodeAndTrafficDate() {
+        String shortCode = "abcdef";
+        String date = simpleDateFormat.format(Date.from(ZonedDateTime.now().toInstant()));
+
+        Optional<Traffic> trafficByShortCodeAndTrafficDate = trafficRepository.findTrafficByShortCodeAndTrafficDate(shortCode, date);
+
+        assertTrue(trafficByShortCodeAndTrafficDate.isPresent());
     }
 
     @Test
     void addValueToTrafficHour() {
+        String shortCode = "abcdef";
+        String date = simpleDateFormatWithTime.format(Date.from(ZonedDateTime.now().toInstant()));
+        LocalDateTime dateTime = LocalDateTime.parse(date, dateTimeFormatter);
+        int hour = dateTime.getHour()-1;
+
+        Long aLong = trafficRepository.increaseTraffic(shortCode, date, hour);
+//        assertTrue(l>0);
     }
 }
