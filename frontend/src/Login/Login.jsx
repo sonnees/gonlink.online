d21b9205dbbbfa778fdaa835a07acf23e65442eb
ToHieuDong <hieudong.dongthanh.02@gmail.com>
2024-06-30 20:36:46 +0700

    login fn

---
+++ frontend/src/Login/Login.jsx
@@ -0,0 +1,34 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+export default function Login() {
+
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/auth/github/login';
+  }
+
+  return (
+    <div className='h-screen overflow-hidden flex'>
+        <div className='w-[30%] bg-slate-400'>
+
+        </div>
+        <div className='w-[70%] my-72'>
+            <div className='flex justify-center'>
+                <div>
+                    <h3 className='font-bold text-xl p-2'>Login using a social network</h3>
+                    
+                    <button onClick={handleLogin} className='font-bold px-5 py-2 border hover:border-sky-600 w-full rounded-md m-2' type="button"><FontAwesomeIcon icon={faGithub} /> Sign in with Github</button>
+
+                    <p className=''>Bằng việc bấm vào nút Đăng nhập ở trên, có nghĩa là bạn đã</p>
+                    <p className=''>đồng ý với <span className='font-bold text-sky-600 mx-1'>Chính sách và điều khoản sử dụng</span> của chúng tôi.</p>
+                </div>
+                
+            </div>
+
+            
+            
+        </div>
+    </div>
+  )
+}
--- frontend/src/Pages/DynamicPage.jsx
+++ frontend/src/Pages/DynamicPage.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 export default function DynamicPage() {
     const { '*': dynamicPath } = useParams();
 
-    window.location.href = 'https://www.youtube.com/watch?v=Ehy3OftSHLs';
+    // window.location.href = 'https://www.youtube.com/watch?v=Ehy3OftSHLs';
 
     return (
         <div>DynamicPage {dynamicPath}</div>
