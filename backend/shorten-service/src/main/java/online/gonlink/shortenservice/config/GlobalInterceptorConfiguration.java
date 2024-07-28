ba0c911c4d4417794b5ea69d288e60a0b1a83ccc
sonnees <sson12131415@gmail.com>
2024-07-28 20:40:08 +0700

    ...

--- backend/shorten-service/src/main/java/online/gonlink/shortenservice/config/GlobalInterceptorConfiguration.java
+++ backend/shorten-service/src/main/java/online/gonlink/shortenservice/config/GlobalInterceptorConfiguration.java
@@ -1,14 +1,21 @@
 package online.gonlink.shortenservice.config;
 
+import io.grpc.ServerInterceptor;
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+import net.devh.boot.grpc.server.serverfactory.GrpcServerConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GlobalInterceptorConfiguration {
 
     @GrpcGlobalServerInterceptor
-    AuthInterceptor authInterceptor() {
+    public AuthInterceptor authInterceptor() {
         return new AuthInterceptor();
     }
 
+    @GrpcGlobalServerInterceptor
+    public RateLimitInterceptor rateLimitInterceptor() {
+        return new RateLimitInterceptor();
+    }
 }
