72d3b1786d188fc8042bcae47638b12ebec6ed75
sonnv <sonnv@unit.com.vn>
2024-11-07 19:51:40 +0700

    --

--- backend/gonlink/src/main/java/online/gonlink/util/ShortUrlUtil.java
+++ backend/gonlink/src/main/java/online/gonlink/util/ShortUrlUtil.java
@@ -8,28 +8,29 @@ import online.gonlink.exception.ResourceException;
 import online.gonlink.exception.enumdef.ExceptionEnum;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Objects;
+import java.util.Date;
 
 public final class ShortUrlUtil {
 
-    public static ShortUrlGenerateDto mapFromGenerateShortCodeRequest(GenerateShortCodeRequest request, PasswordEncoder passwordEncoder){
-        return getShortUrlGenerateDto(passwordEncoder, request.getZoneId(), request.getShortCode(), request.getOriginalUrl(), request.getAlias(), request.getDesc(), request.getTimeExpired(), request.getPassword(), request.getMaxUsage());
+    public static ShortUrlGenerateDto mapFromGenerateShortCodeRequest(SimpleDateFormat simpleDateFormatWithTime, GenerateShortCodeRequest request, PasswordEncoder passwordEncoder){
+        return getShortUrlGenerateDto(simpleDateFormatWithTime, passwordEncoder, request.getZoneId(), request.getShortCode(), request.getOriginalUrl(), request.getAlias(), request.getDesc(), request.getTimeExpired(), request.getPassword(), request.getMaxUsage());
     }
 
-    public static ShortUrlGenerateDto mapFromGenerateShortCodeAccountRequest(GenerateShortCodeAccountRequest request, PasswordEncoder passwordEncoder){
-        return getShortUrlGenerateDto(passwordEncoder, request.getZoneId(), request.getShortCode(), request.getOriginalUrl(), request.getAlias(), request.getDesc(), request.getTimeExpired(), request.getPassword(), request.getMaxUsage());
+    public static ShortUrlGenerateDto mapFromGenerateShortCodeAccountRequest(SimpleDateFormat simpleDateFormatWithTime, GenerateShortCodeAccountRequest request, PasswordEncoder passwordEncoder){
+        return getShortUrlGenerateDto(simpleDateFormatWithTime, passwordEncoder, request.getZoneId(), request.getShortCode(), request.getOriginalUrl(), request.getAlias(), request.getDesc(), request.getTimeExpired(), request.getPassword(), request.getMaxUsage());
     }
 
-    private static ShortUrlGenerateDto getShortUrlGenerateDto(PasswordEncoder passwordEncoder, String zoneId, String shortCode, String originalUrl, String alias, String desc, String timeExpired, String password, long maxUsage) {
+    private static ShortUrlGenerateDto getShortUrlGenerateDto(SimpleDateFormat simpleDateFormatWithTime, PasswordEncoder passwordEncoder, String zoneId, String shortCode, String originalUrl, String alias, String desc, String timeExpired, String password, long maxUsage) {
         String exp = null;
         if(!timeExpired.equals("")){
             ZonedDateTime now = ZonedDateTime.now(ZoneId.of(zoneId));
             ZonedDateTime timeExpiredZonedDateTime = ZonedDateTime.parse(timeExpired).withZoneSameInstant(ZoneId.of(zoneId));
             if(timeExpiredZonedDateTime.isBefore(now))
                 throw new ResourceException(ExceptionEnum.TIME_EXPIRED.name(), null);
-            exp = timeExpiredZonedDateTime.toString();
+            exp = simpleDateFormatWithTime.format(Date.from(timeExpiredZonedDateTime.toInstant()));
         }
         return new ShortUrlGenerateDto(
                 ZonedDateTime.now(ZoneId.of(zoneId)),
